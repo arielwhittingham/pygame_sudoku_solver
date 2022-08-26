4832b5842a6ee2c0addb6d4f40fc0b2d112a8991
arielwhittingham <contact.ariel.whittingham@gmail.com>
2022-08-26 01:52:54 -0400

    first commit

--- sudoku/browser.py
+++ sudoku/browser.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from bs4 import BeautifulSoup
 import chromedriver_autoinstaller
+
 chromedriver_autoinstaller.install()
 
 SudokuConfig = list[list[int]]
@@ -18,17 +19,25 @@ def get_screen_dimensions():
 
 
 class Browser:
-    difficulties = {1: "Easy", 2: "Medium", 3: 'Hard', 4: 'Evil'}
-    _starting_url: str = "https://websudoku.com/"
+    difficulties = {1: "Super easy",
+                    2: "Very easy",
+                    3: "Easy",
+                    4: "Medium",
+                    5: "Hard",
+                    6: "Harder",
+                    7: "Very hard",
+                    8: "Super hard",
+                    9: "Impossible"}
+
+    _starting_url: str = "https://menneske.no/sudoku/eng/random.html"
     _half_screen_width = get_screen_dimensions()['width'] // 2
     _selenium_timeout_limit = 3
 
     def __init__(self):
         self.difficulty = None
-        self.base_url = self._find_base_url()
-        # self.driver = webdriver.Chrome(ChromeDriverManager().install())
+        self.base_url = Browser._starting_url
         self.driver = webdriver.Chrome()
-        self.driver.set_window_position(Browser._half_screen_width,0)
+        self.driver.set_window_position(Browser._half_screen_width, 0)
         self.driver.set_window_size(Browser._half_screen_width, Browser._half_screen_width)
 
     """
@@ -36,25 +45,30 @@ class Browser:
         https://www.programiz.com/python-programming/property 
     """
 
-    def _find_base_url(self):
-        url_data = urllib.request.urlopen(Browser._starting_url)
-        html = BeautifulSoup(url_data, "html.parser")
-        frameset = html.find("frameset")
-        for frame in frameset:
-            try:
-                base_url = frame["src"]
-                return base_url
-            except TypeError:
-                continue
+    # def _find_base_url(self):
+    #     url_data = urllib.request.urlopen(Browser._starting_url)
+    #     html = BeautifulSoup(url_data, "html.parser")
+    #     frameset = html.find("frameset")
+    #     for frame in frameset:
+    #         try:
+    #             base_url = frame["src"]
+    #             return base_url
+    #         except TypeError:
+    #             continue
 
     def set_difficulty(self, test: bool = True):
         if not test:
 
-            print("Select Difficulty.\n",
-                  "1: Easy\n",
-                  "2: Medium\n",
-                  "3: Hard\n",
-                  "4: Evil")
+            print("Select Difficulty.\n"
+                  "1: Super easy\n"
+                  "2: Very easy\n"
+                  "3: Easy\n"
+                  "4: Medium\n"
+                  "5: Hard\n"
+                  "6: Harder\n"
+                  "7: Very hard\n"
+                  "8: Super hard\n"
+                  "9: Impossible")
             while True:
                 try:
                     diff = input(" Enter number corresponding to desired difficulty: ")
@@ -68,7 +82,7 @@ class Browser:
 
             self.difficulty = int(diff)
         else:
-            self.difficulty = 4
+            self.difficulty = 9
 
     def get_difficulty(self) -> str:
         if self.difficulty is not None:
@@ -76,13 +90,7 @@ class Browser:
 
     def _make_url(self) -> str:
         self.set_difficulty()
-        suffix = {
-            1: "",
-            2: "level=2",
-            3: "level=3",
-            4: "level=4"
-        }
-        return self.base_url + suffix[self.difficulty]
+        return f"{self.base_url}?diff={self.difficulty}"
 
     def open_url(self):
 
@@ -96,8 +104,7 @@ class Browser:
         try:
             self.open_url()
             html = BeautifulSoup(self.driver.page_source, "html.parser")
-            html = html.find("table", {"id": "puzzle_grid"})
-            table = html.find('tbody')
+            table = html.find("table", {"id": "main"}).find("div", {"id": "bodycol"}).find("tbody")
             return table
         except RuntimeError as re:
             print("HTML Not Returned correctly", re.args())
@@ -109,7 +116,7 @@ class Browser:
             intermediate_list = []
             for j, col in enumerate(row.find_all("td")):
                 try:
-                    value = int(col.contents[0].attrs['value'])
+                    value = int(col.contents[0])
                     intermediate_list.append(value)
                     # print(value, end=" | ")
                 except:
@@ -124,7 +131,8 @@ class Browser:
 
 def main():
     browser = Browser()
-    board = browser.make_board()
+    browser.open_url()
+
 
 
 if __name__ == "__main__":
